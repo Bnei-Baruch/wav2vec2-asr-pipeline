1787f6a20f3e03f3e13ee8b8e926683c91e1ef08
davgur <davgur.ru@gmail.com>
2026-05-05 17:58:30 +0300

    Update dataset directory path in constants.py for consistency

--- wisper/constants.py
+++ wisper/constants.py
@@ -2,7 +2,7 @@ BASE_MODEL_ID = "ivrit-ai/whisper-large-v3"
 LANGUAGE = "he"
 TASK = "transcribe"
 
-DATASET_DIR = "./dataset_whisper"
+DATASET_DIR = "./dataset"
 ROW_DATA_DIR = "./row_data"
 MODEL_DIR = "./models/whisper-large-v3-he"
 
